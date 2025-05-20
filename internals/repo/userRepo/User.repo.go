41a2d7a5389acfcd9a52264c6e2fd7279b128d61
farhankhan <farhankhan@farhankhans-MacBook-Air.local>
2025-05-20 15:59:44 +0530

    addes interface for user repo

--- internals/repo/userRepo/User.repo.go
+++ internals/repo/userRepo/User.repo.go
@@ -11,6 +11,7 @@ type UserRepoInterface interface {
 	CreateUser(ctx context.Context, user *model.User) error
 	GetUserByID(ctx context.Context, userID uuid.UUID) (*model.User, error)
 	GetUserByEmail(ctx context.Context, email string) (*model.User, error)
-	UpdateUser(ctx context.Context, user *model.User) error
+	UpdateUser(ctx context.Context, userID uuid.UUID, name string, profilepic string) error
 	DeleteAccount(ctx context.Context, userID uuid.UUID) error
+	UpdateRefreshToken(ctx context.Context, userID uuid.UUID, refreshToken string) error
 }
