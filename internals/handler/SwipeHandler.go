1050b3a36f2306ace2817dbddcc46fcb6a17ce15
farhankhan <farhankhan@farhankhans-MacBook-Air.local>
2025-05-22 23:18:00 +0530

    Added SwipeUser in handler

--- internals/handler/SwipeHandler.go
+++ internals/handler/SwipeHandler.go
@@ -17,6 +17,8 @@ func NewSwipeHandler(service *service.SwipeService) *SwipeHandler {
 		service: service,
 	}
 }
+
+// /api/swipe/next
 func (hs *SwipeHandler) GetNextUnswipedUser(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	userIDStr := r.Context().Value("USERID").(string)
@@ -35,4 +37,32 @@ func (hs *SwipeHandler) GetNextUnswipedUser(w http.ResponseWriter, r *http.Reque
 		return
 	}
 	json.NewEncoder(w).Encode(user)
+}
+
+// POST: api/swipe/
+func (hs *SwipeHandler) SwipeUser(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	userIDStr := r.Context().Value("USERID").(string)
+	if userIDStr == "" {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+	userID, err := uuid.Parse(userIDStr)
+	if err != nil {
+		http.Error(w, "user id is not valid", http.StatusUnauthorized)
+		return
+	}
+	var req struct {
+		SwipedID uuid.UUID `json:"swiped_id"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid payload", http.StatusBadRequest)
+		return
+	}
+	if err := hs.service.SaveSwipes(ctx, userID, req.SwipedID); err != nil {
+		http.Error(w, "Failed to swipe", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"message": "Swipe saved"})
 }
