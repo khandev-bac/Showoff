59380d3c6c548436c226a64e03ec44a1d8526c5b
farhankhan <farhankhan@farhankhans-MacBook-Air.local>
2025-05-22 23:08:41 +0530

    Added GetNextUnSwiped handler

--- internals/handler/SwipeHandler.go
+++ internals/handler/SwipeHandler.go
@@ -1,6 +1,12 @@
 package handler
 
-import "exceapp/internals/service"
+import (
+	"encoding/json"
+	"exceapp/internals/service"
+	"net/http"
+
+	"github.com/google/uuid"
+)
 
 type SwipeHandler struct {
 	service *service.SwipeService
@@ -10,4 +16,23 @@ func NewSwipeHandler(service *service.SwipeService) *SwipeHandler {
 	return &SwipeHandler{
 		service: service,
 	}
+}
+func (hs *SwipeHandler) GetNextUnswipedUser(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	userIDStr := r.Context().Value("USERID").(string)
+	if userIDStr == "" {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+	userID, err := uuid.Parse(userIDStr)
+	if err != nil {
+		http.Error(w, "user id is not valid", http.StatusUnauthorized)
+		return
+	}
+	user, err := hs.service.GetUnswippedUsers(ctx, userID)
+	if err != nil {
+		http.Error(w, "No profiles left", http.StatusNotFound)
+		return
+	}
+	json.NewEncoder(w).Encode(user)
 }
